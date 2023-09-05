96ed12a88da137582784ddbbdf51f69eb894a0f6
unknown <ahmeddamess42@gmail.com>
2023-09-05 07:50:35 +0100

    Adding text

--- pages/_document.js
+++ pages/_document.js
@@ -1,56 +1,20 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import React from 'react';
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/favicon/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/favicon/site.webmanifest" />
-          <meta charSet="UTF-8" />
-          <title>Your Page Title</title>
+const YourPage = () => {
+  return (
+    <div>
+      {/* Existing content of your page */}
+      <h1>Welcome to Your Page</h1>
+      <p>Some existing content here...</p>
 
-          {/* Google AdSense */}
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-          <script>
-            {`
-              (adsbygoogle = window.adsbygoogle || []).push({
-                google_ad_client: "YOUR_AD_CLIENT_ID",
-                enable_page_level_ads: true
-              });
-            `}
-          </script>
-        </Head>
-        <body>
-          {/* Add the provided text here */}
-          <p>
-            Get free thumbnail image of any YouTube videos in Full HD(1080), HD
-            (720), SD, and also in small size...
-            {/* Insert the rest of your provided text here */}
-          </p>
+      {/* Add the provided text here */}
+      <p>
+        Get free thumbnail image of any YouTube videos in Full HD(1080), HD
+        (720), SD, and also in small size...
+        {/* Insert the rest of your provided text here */}
+      </p>
+    </div>
+  );
+};
 
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
-
-export default MyDocument;
+export default YourPage;
